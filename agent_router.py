7a9eb93f7a22528b7195fd88d27b880745ffeb5b
RajGothi <rajgothi9@gmail.com>
2025-04-25 14:43:46 +0000

    agent router

--- agent_router.py
+++ agent_router.py
@@ -12,14 +12,14 @@ def route_query_stream(image, query, context=""):
             context
             + f"\nDecide if the user's message is about a property issue (e.g., water damage, mold, cracks, poor lighting, broken fixtures) or tenancy law related FAQ (e.g., rent, landlord rights, agreements, tenant responsibilities, and rental processes.)."
             + f"\nMessage: '{query}'"
-            + "\nRespond with either 'issue', 'FAQ', or 'unclear'."
+            + "\nRespond with either 'issue', 'FAQ', or 'unclear'. Not explanation"
         )
 
         classification = llm_router.invoke(classification_prompt).strip().lower()
         print("classification", classification)
         if "issue" in classification:
             yield from analyze_issue_stream(None, query, context)
-        elif "FAQ" in classification:
+        elif "FAQ" or "faq" in classification:
             yield from answer_faq(query, context)
         else:
             yield "Could you clarify if this is about a property issue or tenancy law?"
